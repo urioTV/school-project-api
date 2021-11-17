2ddf8f8f4309167f0e2d5e117357fd9da3b78214
urioTV <uriootv@protonmail.com>
2021-11-17 18:46:23 +0100

    Subreddit images

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 #[macro_use] extern crate rocket;
+use serde_json::{Value};
+use rand::Rng;
 
 #[get("/")]
-fn index() -> &'static str {
+async fn index() -> &'static str {
     "Hello, world!"
 }
-
-#[get("/<name>")]
-fn hello(name: String) -> String {
-    format!("Hello, {}!", name)
+#[get("/reddit/<sub>")]
+async fn reddit(sub: String) -> String {
+    let suburl = format!("https://www.reddit.com/r/{}.json?sort=hot", sub);
+    let subjson = reqwest::get(&suburl).await.unwrap().text().await.unwrap();
+    let subdata: Value = serde_json::from_str(&subjson).unwrap();
+    let sublen = subdata["data"]["dist"].as_u64().unwrap() as usize;
+    if sublen == 0 {
+        return format!("No posts found for {} or you typed it wrong", sub);
+    }
+    let rndnum = rand::thread_rng().gen_range(0..sublen);
+    format!("{}", subdata["data"]["children"][rndnum]["data"]["url"])
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
-                .mount("/hello", routes![hello])
+
+#[rocket::main]
+async fn main() {
+    rocket::build().mount("/", routes![index, reddit])
+                .launch()
+                .await.expect("Failed to launch rocket");
 }
