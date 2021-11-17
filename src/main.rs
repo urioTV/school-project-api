89e8b76f9dfc2db620b624e336611f9ad0a0d69b
urioTV <uriootv@protonmail.com>
2021-11-17 17:36:14 +0100

    _

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/<name>")]
+fn hello(name: String) -> String {
+    format!("Hello, {}!", name)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+                .mount("/hello", routes![hello])
 }
